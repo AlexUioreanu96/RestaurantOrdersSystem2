785305e281b7f74cdf88d1275bb2711f65bb0eeb
Alex Uioreanu <ext_>
2023-03-22 10:43:05 +0200

    First commit

---
+++ src/main/java/com/example/RestaurantOrdersSystem2/RestaurantOrdersSystem2Application.java
@@ -0,0 +1,13 @@
+package com.example.RestaurantOrdersSystem2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantOrdersSystem2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantOrdersSystem2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/RestaurantOrdersSystem2/RestaurantOrdersSystem2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RestaurantOrdersSystem2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantOrdersSystem2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
